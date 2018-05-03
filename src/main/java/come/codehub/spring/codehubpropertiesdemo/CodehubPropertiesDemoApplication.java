81cd87906f366258a90c433594f63db951245c80
christos <chrisperis@gmail.com>
2018-05-04 01:00:20 +0300

    master project structure

---
+++ src/main/java/come/codehub/spring/codehubpropertiesdemo/CodehubPropertiesDemoApplication.java
@@ -0,0 +1,12 @@
+package come.codehub.spring.codehubpropertiesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehubPropertiesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodehubPropertiesDemoApplication.class, args);
+    }
+}

02596f50bdd74431e927c6bfd32aa97f974fe617
christos <chrisperis@gmail.com>
2018-05-04 01:53:22 +0300

    1-property-injection-xml-placeholder Initial example for <context:property-placeholder/> way

--- src/test/java/come/codehub/spring/codehubpropertiesdemo/CodehubPropertiesDemoApplicationTests.java
+++ src/test/java/come/codehub/spring/codehubpropertiesdemo/CodehubPropertiesDemoApplicationTests.java
@@ -2,15 +2,29 @@ package come.codehub.spring.codehubpropertiesdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration("classpath:application-context.xml")
 public class CodehubPropertiesDemoApplicationTests {
 
+    @Autowired
+    private Environment env;
+
+    @Value("${key.something}")
+    private String injectedProperty;
+
     @Test
-    public void contextLoads() {
+    public void givenContext_noException() {
+        System.out.println("value injected via @Value:" + injectedProperty);
+        System.out.println("value injected via @Value:" + env.getProperty("key.something"));
     }
 
 }

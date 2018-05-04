9b02ba1c3c2e0dd8bee5ae3665eaea049da872ff
christos <chrisperis@gmail.com>
2018-05-04 03:12:34 +0300

    1_2-property-injection-Java-config Comments about PropertySourcesPlaceholderConfigurer

--- src/main/java/come/codehub/spring/codehubpropertiesdemo/config/PropertiesConfig.java
+++ src/main/java/come/codehub/spring/codehubpropertiesdemo/config/PropertiesConfig.java
@@ -1,8 +1,11 @@
 package come.codehub.spring.codehubpropertiesdemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @PropertySource("classpath:bar.properties")
@@ -12,4 +15,16 @@ public class PropertiesConfig {
     public PropertiesConfig() {
         super();
     }
+
+    /*
+        The PropertySourcesPlaceholderConfigurer is used to programmatically register more than one
+        property files in the context. It does not add properties to Environment like @PropertySource
+     */
+
+/*    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new ClassPathResource("foo.properties"));
+        return configurer;
+    }*/
 }
